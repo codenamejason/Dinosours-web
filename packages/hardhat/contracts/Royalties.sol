04457dff61efdf5e38e50c76d8dd0d0d0499a208
Jaxcoder <codenamejason@outlook.com>
2022-03-06 16:54:35 -0500

    add royalties contract

--- packages/hardhat/contracts/Royalties.sol
+++ packages/hardhat/contracts/Royalties.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.7.6;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Royalties {
     using SafeERC20 for IERC20;
@@ -11,20 +11,19 @@ contract Royalties {
 
     // Royalties distro percentages
     uint256 private buidlguidl = 10;
-    uint256 private jax = 80;
+    uint256 private dinosours = 80;
     uint256 private ethana = 10;
 
     // Royalties distro addresses
-    address private buidlguidlAddress = 0x5C37b9f5324bAFB44E10e35E9cB035425F24Ee4E;
-    address private jaxAddy = 0xE6995fDB691aad061e45df31A7C407792A52565A;
+    address private buidlguidlAddress = 0x97843608a00e2bbc75ab0C1911387E002565DEDE;
+    address private dinosoursAddy = 0x695392F1f7900bBB8bcc2Fc6991c22FCb0794342;
     address private ethanaAddress = 0xA9EC89f38a1dC912F1726EE66409a840fFc6a709;
 
     modifier canWithdraw() {
         require(
-            msg.sender == bannersWarChestAddy ||
-                msg.sender == ghostAddy ||
-                msg.sender == jaxAddy ||
-                msg.sender == fahimAddy
+                msg.sender == buidlguidlAddress ||
+                msg.sender == dinosoursAddy ||
+                msg.sender == ethanaAddress
         );
         _;
     }
@@ -48,30 +47,22 @@ contract Royalties {
         uint256 amountToSplit,
         address from
     ) internal {
-        uint256 amountForGhost = uint256(amountToSplit.div(100).mul(ghost));
-        uint256 amountForJax = uint256(amountToSplit.div(100).mul(jax));
-        uint256 amountForFahim = uint256(amountToSplit.div(100).mul(fahim));
-        uint256 amountForBanners = uint256(amountToSplit.div(100).mul(banners));
+        uint256 amountForBuidlGuidl = uint256(amountToSplit.div(100).mul(buidlguidl));
+        uint256 amountForJax = uint256(amountToSplit.div(100).mul(dinosours));
+        uint256 amountForEthana = uint256(amountToSplit.div(100).mul(ethana));
 
         if (token != address(0)) {
-            IERC20(token).safeTransferFrom(from, ghostAddy, amountForGhost);
-            IERC20(token).safeTransferFrom(from, jaxAddy, amountForJax);
-            IERC20(token).safeTransferFrom(from, fahimAddy, amountForFahim);
-            IERC20(token).safeTransferFrom(
-                from,
-                bannersWarChestAddy,
-                amountForBanners
-            );
+            IERC20(token).safeTransferFrom(from, buidlguidlAddress, amountForBuidlGuidl);
+            IERC20(token).safeTransferFrom(from, dinosoursAddy, amountForJax);
+            IERC20(token).safeTransferFrom(from, ethanaAddress, amountForEthana);
         } else {
             bool success;
-            (success, ) = ghostAddy.call{value: amountForGhost}("");
-            require(success, "Ghost transaction failed");
-            (success, ) = jaxAddy.call{value: amountForJax}("");
-            require(success, "Jax transaction failed");
-            (success, ) = fahimAddy.call{value: amountForFahim}("");
-            require(success, "Fahim transaction failed");
-            (success, ) = bannersWarChestAddy.call{value: amountForBanners}("");
-            require(success, "Banners transaction failed");
+            (success, ) = buidlguidlAddress.call{value: amountForBuidlGuidl}("");
+            require(success, "BG transaction failed");
+            (success, ) = dinosoursAddy.call{value: amountForJax}("");
+            require(success, "Dino transaction failed");
+            (success, ) = ethanaAddress.call{value: amountForEthana}("");
+            require(success, "Ethana transaction failed");
         }
     }
 
